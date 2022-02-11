d744df92d91838d055e39793e214f606427fbd76
Oladugba Demilade <demiladeoladugba@gmail.com>
2022-02-11 16:18:29 +0100

    user input

--- main.go
+++ main.go
@@ -4,8 +4,8 @@ import "fmt"
 
 func main() {
 	conferenceName := "Crush Conference" // Syntactic sugar
-	const conferenceTickets = 50
-	var remainingTickets = 50
+	const conferenceTickets int = 50
+	var remainingTickets uint= 50
 
 	fmt.Printf("conferenceName is %T, conferenceTickets is %T and remainingTickets is %T\n", conferenceName, conferenceTickets, remainingTickets)
 
@@ -13,11 +13,24 @@ func main() {
 	fmt.Printf("We have a total of %v conference tickets and %v are still available\n", conferenceTickets, remainingTickets)
 	fmt.Println("Get your tickets here to attend😋")
 
-	var userName string
+	var firstName string
+	var lastName string
+	var email string
 	var userTickets int
 
-	userName = "Demi"
 	userTickets = 4
 
-	fmt.Printf("User %v has %v tickets\n", userName, userTickets)
+	fmt.Println("Enter your First Name: ")
+	fmt.Scan(&firstName)
+	
+	fmt.Println("Enter your Last Name: ")
+	fmt.Scan(&lastName)
+	
+	fmt.Println("Enter your Email: ")
+	fmt.Scan(&email)
+	
+	fmt.Println("How many tickets do you want to book: ")
+	fmt.Scan(&userTickets)
+
+	fmt.Printf("Thank you, %v %v. We received your order to book %v tickets. You will a confirmation email at %v.\n", firstName, lastName, email, userTickets)
 }
