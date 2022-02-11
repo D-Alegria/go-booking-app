ae77b14168f07a4dc22e41d49805697d90557665
Oladugba Demilade <demiladeoladugba@gmail.com>
2022-02-11 14:26:30 +0100

    Data types

--- main.go
+++ main.go
@@ -3,10 +3,21 @@ package main
 import "fmt"
 
 func main() {
-	var conferenceName = "Crush Conference"
+	conferenceName := "Crush Conference" // Syntactic sugar
 	const conferenceTickets = 50
 	var remainingTickets = 50
+
+	fmt.Printf("conferenceName is %T, conferenceTickets is %T and remainingTickets is %T\n", conferenceName, conferenceTickets, remainingTickets)
+
 	fmt.Printf("Welcome to %v Booking Application\n", conferenceName)
 	fmt.Printf("We have a total of %v conference tickets and %v are still available\n", conferenceTickets, remainingTickets)
 	fmt.Println("Get your tickets here to attend😋")
+
+	var userName string
+	var userTickets int
+
+	userName = "Demi"
+	userTickets = 4
+
+	fmt.Printf("User %v has %v tickets\n", userName, userTickets)
 }
